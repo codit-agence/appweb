c3e2910b07fed88a3342732f1d5b08a39a64ed7c
agencecodit <agencecodit@gmail.com>
2020-10-23 19:57:54 +0200

    first commit agence codit

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
     email = models.EmailField()
     message= models.TextField()
     title=models.CharField(max_length=256, default='contact web')
-    tel=models.IntegerField(max_length=10)
+    tel=models.IntegerField()
     ville=models.CharField(max_length=100)
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Contact(models.Model):
 class ContactInscription(models.Model):
     name    =models.CharField(max_length=100)
     email   = models.EmailField()
-    tel     =models.IntegerField(max_length=10)
+    tel     =models.IntegerField()
     ville   =models.CharField(max_length=100)
     message = models.CharField(max_length=256)
 
--- formation/views.py
+++ formation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Offres
 
+
 def formation_index(request):
     offres = Offres.objects.all()
     return render(request,'offres.html', {'offres':offres} )
